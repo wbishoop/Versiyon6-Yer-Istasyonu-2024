e381bf42f05fae4e1c8502be184058734db8c40f
Ahmet Selim <141772001+wbishoop@users.noreply.github.com>
2024-08-26 17:01:04 +0300

    main.py
    Main dosya

---
+++ main.py
@@ -0,0 +1,34 @@
+from customtkinter import *
+from pages import Anasayfa, YerIstasyonu, VeriCek
+
+
+# -----Genel Yapı ve Sayfa Sistemi-----------------------------------------------------------
+class Versiyon6YerIstasyonu(CTk):
+    def __init__(self, *args, **kwargs):
+        CTk.__init__(self, *args, **kwargs)
+        container = CTkFrame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (Anasayfa, YerIstasyonu, VeriCek):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Anasayfa)
+        # print("w:", self.winfo_width(), "h:", self.winfo_height())
+
+    # -----------Yeni Sayfaya Geçiş -----------------------------------------------------
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        self.iconbitmap("V6-logo.ico")
+        self.title("Yer İstasyonu Yazılımı / Versiyon6 Roket Takımı")
+        self.resizable(FALSE, FALSE)
+
+
+if __name__ == "__main__":
+    app = Versiyon6YerIstasyonu()
+    app.mainloop()
